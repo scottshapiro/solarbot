949708f8830c8aa25f7d134973bd969a93de2f29
Scott Shapiro (Product Marketing) <sshapiro@sshapiro-mbp2.dhcp.thefacebook.com>
2017-03-07 21:19:34 -0800

    added current power

--- solar.py
+++ solar.py
@@ -9,9 +9,8 @@ def solar():
         # response ='{"system_id":45157,"modules":14,"size_w":2660,"current_power":3,"energy_today":11491,"energy_lifetime":21668606,"summary_date":"2017-03-07","source":"microinverters","status":"normal","operational_at":1321406693,"last_report_at":1488939265,"last_interval_end_at":1488939052}'
 	response = urlopen(request)
 	solar = json.loads(response.read())
-	message = str(solar['energy_today']) + " Wh were produced today"
+	message = str(solar['energy_today']) + "Wh were produced today.\n" + str(solar['current_power']) + "W this moment."
 	print message
 	return message
     except URLError, e:
         print 'No kittez. Got an error code:', e
-
