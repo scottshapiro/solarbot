010ca36e5245ac76593f2224325475dbefc60b26
Scott Shapiro (Product Marketing) <sshapiro@sshapiro-mbp2.dhcp.thefacebook.com>
2017-03-07 21:02:00 -0800

    solar function reports daily production of energy

--- solar.py
+++ solar.py
@@ -1,12 +1,15 @@
 from urllib2 import Request, urlopen, URLError
+import json
 
 def solar():
 
     request = Request('https://api.enphaseenergy.com/api/v2/systems/45157/summary?key=3fbdaa7269e667f13a87d33c2b8d5e09&user_id=4f4449314e6a63350a')
 
     try:
+        # response ='{"system_id":45157,"modules":14,"size_w":2660,"current_power":3,"energy_today":11491,"energy_lifetime":21668606,"summary_date":"2017-03-07","source":"microinverters","status":"normal","operational_at":1321406693,"last_report_at":1488939265,"last_interval_end_at":1488939052}'
 	response = urlopen(request)
-	solar = response.read()
-	print solar
+	solar = json.loads(response.read())
+	print str(solar['energy_today']) + " Wh were produced today"
     except URLError, e:
         print 'No kittez. Got an error code:', e
+
