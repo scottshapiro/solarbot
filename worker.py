a882cf1715cb26224598a2d747d81818f1cf734d
Scott Shapiro <scott@scottshapiro.com>
2017-03-19 10:36:34 -0700

    fixed rq to use redis to go

---
+++ worker.py
@@ -0,0 +1,15 @@
+import os
+import redis
+from rq import Worker, Queue, Connection
+
+listen = ['high', 'default', 'low']
+
+redis_url = os.getenv('REDISTOGO_URL', 'redis://redistogo:cdc98dc1d537aa1bbb8df876e45836f9@koi.redistogo.com:11883/')
+
+conn = redis.from_url(redis_url)
+
+if __name__ == '__main__':
+    with Connection(conn):
+	print 'doing work'
+        worker = Worker(map(Queue, listen))
+        worker.work()
